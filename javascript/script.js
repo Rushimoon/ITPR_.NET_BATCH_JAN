0646d39270b8de6c4ce23af867429c5f17ed7a37
Rushimoon <rushikeshmoon1998@gmail.com>
2024-06-26 12:35:14 +0530

    js started

---
+++ javascript/script.js
@@ -0,0 +1,40 @@
+var x=12;
+
+let z=12;
+const y=12;
+
+z=60
+
+console.log(y)
+
+
+let age1=12
+const age2=12
+
+console.log(age2%2)
+
+
+
+//compar
+
+console.log(age1===age2)
+
+age1+=8
+console.log(age1)
+
+
+//logical
+
+console.log(!false)
+
+
+
+ans=27
+
+console.log(ans--)//20
+++ans
+console.log(ans)//21
+--ans
+console.log(ans)//19
+++ans
+console.log(ans)//21
